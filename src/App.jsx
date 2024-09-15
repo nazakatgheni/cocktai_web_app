a9afecf41d72b1b22fef4176ac0bf04b83562872
Nazakat Gheni <nazakatgheni@ghenis-MacBook-Air.local>
2024-09-14 20:36:29 -0400

    code changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Form from './components/Form';
 import Recipe from './components/Recipe';
 import CocktailPage from './components/CocktailPage';
